7d607df6f0bb76663feb9b13aa6dc0b5e171d76f
KingScroll <69558037+KingScroll@users.noreply.github.com>
2021-05-20 15:18:48 -0400

    Header & Footer Update + Dark Mode Mechanics

---
+++ themes/Connectchain/static/js/themeswitch.js
@@ -0,0 +1,17 @@
+const btn = document.getElementById("themeswitcher");
+
+const currentTheme = localStorage.getItem("theme");
+if (currentTheme == "dark") {
+  document.body.classList.add("dark-theme");
+}
+
+btn.addEventListener("click", function () {
+  document.body.classList.toggle("dark-theme");
+
+  let theme = "light";
+  if (document.body.classList.contains("dark-theme")) {
+    theme = "dark";
+  }
+  localStorage.setItem("theme", theme);
+  location.reload();
+});
